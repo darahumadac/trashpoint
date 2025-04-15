00f6a48c7e956867133a9986c217f0796017f975
darah <darahfumadac@gmail.com>
2025-04-16 01:34:11 +0800

    updated splash screen

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -5,20 +5,23 @@ import { useFonts } from "expo-font";
 
 export default function Splash() {
   const [loaded] = useFonts({
-    "ChelseaMarket": require("../assets/fonts/ChelseaMarket-Regular.ttf"),
+    ChelseaMarket: require("../assets/fonts/ChelseaMarket-Regular.ttf"),
   });
   return (
     <View style={styles.splashContainer}>
       <LottieView
         source={require("../assets/trashpoint-splash.json")}
         autoPlay
-        loop
+        // loop
         resizeMode="cover"
         style={styles.logoContainer}
-        // onAnimationFinish={}
       />
       <View style={styles.appNameContainer}>
-        {loaded && <Text style={styles.appName}>TrashPoint</Text>}
+        {loaded && (
+          <>
+            <Text style={styles.appNameGreen}>TrashPoint</Text>
+          </>
+        )}
       </View>
     </View>
   );
@@ -28,19 +31,25 @@ const styles = StyleSheet.create({
   appNameContainer: {
     position: "absolute",
     alignSelf: "center",
+    alignItems: "center",
     justifyContent: "flex-end",
-    height: 250,
+    height: 220,
+    width: "100%",
   },
-  appName: {
-    fontSize: 60,
-    color: "#5a8226",
-    fontFamily: "ChelseaMarket"
+  appNameGreen: {
+    fontSize: 55,
+    textShadowRadius: 2,
+    textShadowColor: "#788777",
+    textShadowOffset: { width: 2, height: 3 },
+    color: "#39d62b",
+    fontFamily: "ChelseaMarket",
+    letterSpacing: 4,
   },
   logoContainer: { position: "relative", height: "50%", top: "-10%" },
   splashContainer: {
     flex: 1,
     flexDirection: "column",
     justifyContent: "center",
-    backgroundColor: "#d8ebc0",
+    backgroundColor: "#caf0c7",
   },
 });
